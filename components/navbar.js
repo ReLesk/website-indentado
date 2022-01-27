1190e34bb4932f03710aaa9555428a601c4176db
relesk <outdaywork@gmail.com>
2022-01-26 19:48:10 -0500

    Changes

--- components/navbar.js
+++ components/navbar.js
@@ -1,31 +1,66 @@
 import Link from "next/link";
+import Button from "./ui/buttons/button";
+
+const buttonMenu = () => {
+  document.getElementById("mobile-menu").classList.toggle("hidden");
+}
 
 export default function Navbar() {
   return (
-    <nav className="bg-transparent">
-      <div className="container mx-auto">
-        <div className="sm:flex justify-around">
-          <a href="#" className="text-slate-900 text-3xl font-bold p-3">
-            in
-          </a>
-          <ul className="text-zinc-900 sm:self-center font-medium text-base">
-            <li className="sm:inline-block">
-              <a href="#" className="p-3 hover:text-pink-600 transition-colors">
-                Inicio
+    <nav className="">
+      <div className="max-w-6xl mx-auto px-4">
+        <div className="flex justify-between">
+          <div className="flex space-x-10">
+            <div>
+              <a
+                href="#"
+                className="flex items-center py-5 px-2 text-gray-700 hover:text-gray-900"
+              >
+                <span className="font-black text-zinc-900 text-2xl">
+                  Indentado
+                </span>
               </a>
-            </li>
-            <li className="sm:inline-block">
-              <a href="#" className="p-3 hover:text-pink-600 transition-colors">
-                Proyectos
-              </a>
-            </li>
-            <li className="sm:inline-block">
-              <a href="#" className="p-3 hover:text-pink-600 transition-colors">
-                Documentación
-              </a>
-            </li>
-          </ul>
+            </div>
+
+            <div className="hidden md:flex items-center space-x-1">
+              <Link href="/">
+                <a className="py-5 px-3 text-zinc-700 hover:text-pink-600 transition-colors">
+                  Proyectos
+                </a>
+              </Link>
+              <Link href="/">
+                <a
+                 
+                  className="py-5 px-3 text-zinc-700 hover:text-pink-600 transition-colors"
+                >
+                  Documentación
+                </a>
+              </Link>
+            </div>
+          </div>
+
+          <div className="hidden md:flex items-center space-x-1">
+            <a href="" className="py-5 px-3 text-zinc-700 hover:text-pink-600 transition-colors">
+              Github
+            </a>
+            <Button href={"/"}>Contacto</Button>
+          </div>
+
+          <div className="md:hidden flex items-center">
+            <button onClick={buttonMenu} className="mobile-menu-button">
+              menu
+            </button>
+          </div>
         </div>
+      </div>
+
+      <div id="mobile-menu" className="mobile-menu hidden md:hidden">
+        <a href="#" className="block py-2 px-4 text-sm hover:bg-gray-200">
+          Features
+        </a>
+        <a href="#" className="block py-2 px-4 text-sm hover:bg-gray-200">
+          Pricing
+        </a>
       </div>
     </nav>
   );
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-import Button from "../components/buttons/button";
+import Button from "../components/ui/buttons/button";
 import Layout from "../components/layout";
 
 export default function Home() {
